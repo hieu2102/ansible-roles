5d3d9f479781f005537516cc4c3f5c46aa62c81d
hieu2102 <hieund2102@gmail.com>
2023-01-15 20:03:20 +0700

    commit

--- roles/confluent/install_confluent_cluster/files/update_listeners.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-NEW_LISTENER="${1}"
-sed -i -r "s|^(listeners=.+)|\1,${NEW_LISTENER}|g" /etc/kafka/server.properties
--- roles/confluent/install_confluent_cluster/files/update_sasl.enabled.mechanisms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-NEW_MECHANISM="${1}"
-
-sed -i -r  "s|^(sasl.enabled.mechanisms=.+)|\1,${NEW_MECHANISM}|g" /etc/kafka/server.properties
